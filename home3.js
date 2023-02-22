763cb65daf788e567cbd4e3add6dd24d12f02154
harsha541995 <116940389+harsha541995@users.noreply.github.com>
2023-02-22 11:13:33 +0530

    Update home3.js

--- home3.js
+++ home3.js
@@ -13,29 +13,24 @@ const ctx = canvas.getContext("2d");
 let H = 0.005*width;
 const Y = 0.005*height;
 let ballArray = [];
-// const ballColor= '#BFBFFF';
-// let ballColor = '#e6f8fb';
-let ballColor;
+let ballColor= "white";
 let lineColor="";
 let ballCount= 200;
 
-let d1=18*H;
-let d2=15*H;
-let d3=10*H;
+let d1=15*H;
+let d2=7*H;
+let d3=3*H;
 
 let radius = 0.5*H;
 
-if(width>700){
-    H=0.005*width;
-    ballCount= 250;
-    radius =0.6*H;
-    // d1=80*H;
-    // d2=40*H;
-    // d3=20*H;
-d1=10*H;
-d2=5*H;
-d3=2.5*H;
-}
+// if(width>700){
+//     H=0.005*width;
+//     ballCount= 250;
+//     radius =0.5*H;
+// d1=10*H;
+// d2=5*H;
+// d3=2.5*H;
+// }
 
 
 
@@ -45,106 +40,67 @@ d3=2.5*H;
 
 
 var grd = ctx.createLinearGradient(0, 0, 0, canvas.height);
-// grd.addColorStop(0, "#4949FF");
-// grd.addColorStop(1, "#0000FF");
 
 grd.addColorStop(0, "#00579c");
 grd.addColorStop(0.8, "#03acc1");
 
 
 class Point{
-    constructor(x,y,size){
-        this.x=x;
-        this.y=y;
-        this.radius=size;
-        this.iy=0;
-        this.ix=0;
+    constructor(x,y){
+this.x=x;
+this.y=y;
+this.A = 10 + 10*Math.random();;
+this.w= 0.0001*(1+Math.random());
+this.k= 2*Math.PI/((Math.floor(50+100*Math.random()))*width/ballCount);
+this.size = (0.5+1*Math.random())*(radius);
+this.velocityX = -1+ 2*Math.random();
+this.ix;
 
-        this.A= 1*Math.random();
-        // this.velocityX = -0.5 + 1*Math.random();;
-        // this.velocityY = -0.5 + 1*Math.random();;
+}
 
-        
-        this.velocityY=  Math.random()*Math.sin(Math.PI*((this.y-(height))/(height)));       ;
-   
-        // if(this.velocityY<0){
+drawArc( color ){
+ctx.save();    
+ctx.fillStyle= color;
+ctx.beginPath();    
+ctx.arc(this.x,this.y,this.size,0,2*Math.PI)
+ctx.fill();
+ctx.restore();
 
-            // this.velocityX =  Math.random()*Math.sin(Math.PI*((this.x-(width))/(width)));
-            this.velocityX = -0.5+Math.random();
+}
 
-        // }else{
-            // this.velocityX =  Math.abs(  Math.random()*Math.sin(Math.PI*((this.x-(width))/(width))));
-
-
-        // }
-            
-            
-
-
-
-
-        }
-       drawArc(ballColor){
-        ctx.save();
-        ctx.fillStyle = ballColor;
-        ctx.translate(this.x,this.y);
-        ctx.scale(scale, scale);
-        ctx.beginPath()
-        ctx.arc(0,0,this.radius, 0, 2*Math.PI);
-        ctx.fill();
-        ctx.restore();
-
-    }
 }
 
 
-for(i=0;i<ballCount;i++){
+for(let i=0;i<ballCount;i++){
 
-    let size = 0.5+Math.random()*radius;
-    // let x = size+ (width-size)*Math.random();
-    // let y = size+ (height-size)*Math.random();
- 
-    let x = size+ i*width/ballCount;
-    let y = size+ i*height/ballCount;    
-      
+let x=i*width/ballCount;
 
-    ballArray.push( new Point(x,y,size));
-    ballArray[i].iy = y;
-    ballArray[i].ix = x;
+    ballArray.push(new Point(x,0))
 
-    console.log( ballArray[i]);
+    let ball = ballArray[i];   
+    ball.ix = x;
+
+
 }
 
-
-
+let t=0
 setInterval(()=>{
 
+    t=t+1;
     ctx.clearRect(0,0,canvas.width,canvas.height);
-
     ctx.fillStyle = grd;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);    
+    ctx.fillRect(0, 0, canvas.width, canvas.height);      
 
-    
-    for(i=0;i<ballArray.length;i++){
-    
-          if(ballArray[i].x<=0 || ballArray[i].x>=canvas.width){
-            ballArray[i].velocityX = -1*ballArray[i].velocityX;
-            // ballArray[i].velocityY = -1*ballArray[i].velocityY;
+ 
 
-        }
-     
-        if(ballArray[i].y<=0 || ballArray[i].y>=canvas.height){
-            // ballArray[i].velocityY = -1*ballArray[i].velocityY;
-            // ballArray[i].velocityX = -1*ballArray[i].velocityX;
 
-        }  
+for(ball of ballArray){
 
-        if(ballArray[i].y<height/3){
-            // ballColor = "#99e4ee";
+
+    if(ball.y<height/3){
         ballColor ="#00bcd4";
-
-           } else if(ballArray[i].y<2*height/3){
-            // ballColor = "#b3ebf2";
+    
+           } else if(ball.y<2*height/3){
             ballColor = "#80deea";
         
            } else {
@@ -152,74 +108,95 @@ setInterval(()=>{
         
            }
 
-           ballArray[i].x += ballArray[i].velocityX;  
-           ballArray[i].y = ballArray[i].iy + ballArray[i].A*Math.sin(Math.PI*(ballArray[i].velocityY*0.1)*(ballArray[i].ix-ballArray[i].x))
 
-        ballArray[i].drawArc(ballColor);    
 
-    }
+    ball.x += ball.velocityX;
+    ball.y = ball.A*Math.sin((ball.w*t)+ball.k*(ball.x-ball.ix))  + ((ball.ix)/width)*(height);
+    // ball.y = height/2- ball.A*10*Math.sin(ball.w*(ball.x-ball.ix));
     
-    for(i=0; i<ballArray.length;i++){
-        let agent = ballArray[i];
-    
-        for( j=0;j<i;j++){
-         let other = ballArray[j]
-    
-         let distance = Math.sqrt(Math.pow((agent.x-other.x),2)+Math.pow((agent.y-other.y),2));
-    
+    ball.drawArc(ballColor);
+
+if(ball.x<0 || ball.x>canvas.width){
+
+ball.velocityX = -ball.velocityX;    
+
+}
+
+
+}
+
+
+for(i=0; i<ballArray.length;i++){
+    let agent = ballArray[i];
+
+    for( j=0;j<i;j++){
+     let other = ballArray[j]
+
+     let distance = Math.sqrt(Math.pow((agent.x-other.x),2)+Math.pow((agent.y-other.y),2));
+
+
+
+     if(distance <= d1 ){
+       
+        if(distance>=d2){
+            if(width>700){
+            lineWidth = 0.1*radius;
+            }else{
+         lineWidth = 0.2*radius;}
+
  
+        }else if(distance<d2&& distance>d3){
+            if(width>700){
+            lineWidth = 0.2*radius;
+            }else{
+         lineWidth = 0.3*radius;}
 
-         if(distance <= d1 ){
-           
-            if(distance>=d2){
-                if(width>700){
-                lineWidth = 0.1*radius;
-                }else{
-             lineWidth = 0.2*radius;}
+
+        }else if(distance<=d3){
+            if(width>700){
+            lineWidth = 0.3*radius;
+            }else{
+         lineWidth = 0.4*radius;}
  
-     
-            }else if(distance<d2&& distance>d3){
-                if(width>700){
-                lineWidth = 0.2*radius;
-                }else{
-             lineWidth = 0.3*radius;}
- 
- 
-            }else if(distance<=d3){
-                if(width>700){
-                lineWidth = 0.3*radius;
-                }else{
-             lineWidth = 0.4*radius;}
-     
-            }
-
-
-           if(ballArray[i].y<height/3){
-         lineColor= "#00bcd4";
-
-           } else if(ballArray[i].y<2*height/3){
-          lineColor="#80deea";
-
-           } else {
-            lineColor= "white";
-        
-           }
-
-                
-            ctx.strokeStyle = lineColor; 
-            ctx.lineWidth = lineWidth;
-            ctx.beginPath();
-            ctx.moveTo(agent.x, agent.y);
-            ctx.lineTo(other.x,other.y);
-            ctx.stroke();
-
-      
-        
-
-        
-         }
         }
-    }
-    }
-    ,50)
+
+
+       if(ballArray[i].y<height/3){
+     lineColor= "#00bcd4";
+
+       } else if(ballArray[i].y<2*height/3){
+      lineColor="#80deea";
+
+       } else {
+        lineColor= "white";
     
+       }
+
+            
+        ctx.strokeStyle = lineColor; 
+        ctx.lineWidth = lineWidth;
+        ctx.beginPath();
+        ctx.moveTo(agent.x, agent.y);
+        ctx.lineTo(other.x,other.y);
+        ctx.stroke();
+
+  
+    
+
+    
+     }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+}
+,50)
